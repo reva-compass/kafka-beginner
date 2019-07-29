74447242a25f6bb5e5e921d5e33fd916f9595f6e
rkandoji <rkandoji@compass.com>
2019-07-29 12:04:19 -0400

    adding consumers

---
+++ src/main/java/com/github/rkandoji/kafka/Consumer.java
@@ -0,0 +1,49 @@
+package com.github.rkandoji.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+
+        Logger LOG = LoggerFactory.getLogger(Consumer.class);
+        String bootstrapServer = "localhost:9092";
+        String groupId = "my-first-application";
+        String topic = "first_topic";
+
+        // create consumer configs
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        // subscribe consumer to topic
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for data
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for(ConsumerRecord<String, String> record : records) {
+                LOG.info("Key:" + record.key() + " Value:" + record.value());
+                LOG.info("Partition:" + record.partition() + " Offset:" + record.offset());
+            }
+
+        }
+    }
+}
