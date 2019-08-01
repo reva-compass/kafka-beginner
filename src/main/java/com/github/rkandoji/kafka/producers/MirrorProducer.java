848709b233b3f42de3d68ec9a155875788cd6252
rkandoji <rkandoji@compass.com>
2019-08-01 07:58:55 -0400

    renaming producer

--- src/main/java/com/github/rkandoji/kafka/producers/MirrorProducer.java
+++ src/main/java/com/github/rkandoji/kafka/producers/MirrorProducer.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 import java.util.Properties;
 import java.util.UUID;
 
-public class AgentProducer {
+public class MirrorProducer {
 
     public static void main(String[] args) {
         System.out.println("Hello there!");
